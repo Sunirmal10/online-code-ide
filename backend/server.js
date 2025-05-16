3edcfffa3ea9cd0da9d8fd7be05474574f37d6aa
SUNIRMAL DAS <sunirmald1996@gmail.com>
2025-05-16 21:21:35 +0530

    update cors again

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 app.use(cors(
   {
-    origin: "https://online-code-ide-hi90.onrender.com"
+    origin: "https://online-code-ide-xv1x.onrender.com"
   }
 ));
 
